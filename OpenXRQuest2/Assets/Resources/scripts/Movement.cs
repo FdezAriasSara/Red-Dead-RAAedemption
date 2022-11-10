342d1c64cc66d1d120f45c296ac88c7729575a8f
Martin Beltran <UO276244@uniovi.es>
2022-11-10 17:47:14 +0100

    Quest2 setup test

---
+++ OpenXRQuest2/Assets/Resources/scripts/Movement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Security;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+///--------------------------------
+///   Author: Victor Alvarez, Ph.D.
+///   University of Oviedo, Spain
+///--------------------------------
+
+public class Movement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 8f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 2f;
+
+    public string goal ="RabbitHole";
+    public string nextScene = "Menu"; 
+
+    Vector3 velocity;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump"))
+            velocity.y = Mathf.Sqrt (jumpHeight * -2f * gravity);
+        
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    public void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.CompareTag(goal))
+        {
+            Debug.Log("I'm Late & Down the Rabbit Hole");
+            SceneManager.LoadScene(nextScene);
+        }        
+    }
+}
