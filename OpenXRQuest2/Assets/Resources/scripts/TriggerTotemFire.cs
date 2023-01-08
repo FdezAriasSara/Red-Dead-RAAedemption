ba24b2d2e998718392d38aa73997ff13cde22e3b
Sara Fernández Arias <uo269546@uniovi.es>
2023-01-08 14:39:06 +0100

    Triggers zonas , fuego

---
+++ OpenXRQuest2/Assets/Resources/scripts/TriggerTotemFire.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/******************************************
+*                                         *
+* Sara Fernández Arias - RAA - 2022-2023  *
+*                                         *
+*******************************************/
+/**
+   Este script hace que se muestre el fuego bajo los totems del altar , aasí como la posición que el jugador debe adquirir para que se muestren las animaciones del hechizo y se termine la misión.
+   */
+public class TriggerTotemFire : MonoBehaviour
+{
+    /**
+    *El fuego aparecerá al poner  la calavera en el altar. Aparecerá también , el marcador de la posición que el jugador debe ocupar.
+    */
+    void OnTriggerStay(Collider collider)
+    {
+
+
+        string tagStr = collider.gameObject.tag;
+
+        if (tagStr.Equals("Ofrenda"))
+        {
+            displayElements(true);
+        }
+
+    }
+
+    /**
+    *El fuego desaparecerá al quitar la calavera del altar.
+    */
+    void OnTriggerLeave(Collider collider)
+    {
+        displayElements(false);
+
+
+    }
+
+    private void displayElements(bool display)
+    {
+        GameObject[] fireObjects = GameObject.FindGameObjectsWithTag("Respawn");
+        foreach (var fire in fireObjects)
+        {
+            fire.SetActive(display);
+        }
+        GameObject playerPosition = GameObject.FindGameObjectsWithTag("PlayerPosition")[0];
+        playerPosition.SetActive(display);
+    }
+}
